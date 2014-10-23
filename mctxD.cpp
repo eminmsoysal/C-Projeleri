1293dde2ec686e2aa3d1a93077b360b865105520
Hüseyin ANLAMAZ <hanlamaz13@posta.pau.edu.tr>
2014-10-23 21:00:57 +0300

    C Projelerim - My C Projects
    Hepsi kendi emeğim olan projelerim - All C Projects made by me

---
+++ mctxD.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <conio.h>
+// BU PROGRAM ÝKÝ SAYIDAN HANGÝSÝNÝN BÜYÜK OLDUGUNU HESAPLAR  
+/*int main ()
+{
+	int sayi1,sayi2;
+	printf("Birinci sayi = ");
+	scanf("%d",&sayi1);
+	printf("Ikinci sayi = ");
+	scanf("%d",&sayi2);
+	if (sayi1>sayi2)
+		printf("\n Buyuk sayi %d",sayi1);
+	else
+	printf("\n Buyuk sayi %d",sayi2);
+	getch ();
+} */
+
+// SAYININ POZITIF NEGATIF VEYA 0 OLDGUNU BULUR HÜSEYÝN
+/*void main ()
+	
+{
+	int a;
+	scanf("%d",&a);
+	if(a<0)
+	printf("a sayisi negatif");
+	else if(a>0)
+	printf("a sayisi pozitif");
+	else
+	printf("a sayisi 0 a esit :D");
+	getch ();
+}*/
+
+// BU PROGRAM VIZE VE FINAL NOTLARINI GÝREREK ORTALAMA ÇIKARIR
+/*void main ()
+{
+float v,f,ort;
+printf("Vize notunu girin = ");
+scanf("%f",&v);
+printf("Final notunu girin = ");
+scanf("%f",&f);
+ort=(v*0.40)+(f*0.60);
+printf("Ortalaman = %5.2f ",ort);
+if (ort>50)
+printf("\nGectin");
+else if (ort<50)
+printf("\nKaldin");
+else
+printf("\n Seni sansli seni");
+getch ();
+}*/
+
+// BU PROGRAM SAYININ TEK VEYA ÇÝFT OLDUGUNU HESPLAR
+/*void main ()
+{
+	int a;
+	printf("Sayiyi girin = ");
+	scanf("%d",&a);
+	if (a%2==0)
+	printf("Sayi cift");
+	else
+	printf("Sayi tek");
+	getch ();
+}*/
+
+// BU PROGRAM GÝRÝLEN 3 SAYININ EN BÜYÜGÜNÜ BULUR HÜSEYÝN
+/*void main ()
+{
+	int a,b,c;
+	printf ("3 sayiyi gir\n ");
+	scanf("%d %d %d",&a,&b,&c);
+	if (a>b&a>c)
+	printf(" %d sayisi en buyuk sayi",a);
+	if (b>a&b>c)
+	printf(" %d sayisi en buyuk sayi",b);
+	if (c>a&c>b);
+	printf(" %d sayisi en buyuk sayi",c);
+	getch ();
+}*/
+ // ( AGIR ÝÞSÝZLÝK URUNU - LUTFEN EVDE DENEMEYÝN :) ) SAYILARI SÖYLER xD 
+/*void main ()
+{
+int a,b,c,d,e;
+printf("1 ile 5 arasindaki tum sayilari gir");
+scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+if (a==1)
+printf("bir\n");
+if (a==2)
+printf("iki\n");
+if (a==3)
+printf("uc\n");
+if (a==4)
+printf("dort\n");
+if (a==5)
+printf("bes\n");
+if (b==1)
+printf("bir\n");
+if (b==2)
+printf("iki\n");
+if (b==3)
+printf("uc\n");
+if (b==4)
+printf("dort\n");
+if (b==5)
+printf("bes\n");
+if (c==1)
+printf("bir\n");
+if (c==2)
+printf("iki\n");
+if (c==3)
+printf("uc\n");
+if (c==4)
+printf("dort\n");
+if (c==5)
+printf("bes\n");
+if (d==1)
+printf("bir\n");
+if (d==2)
+printf("iki\n");
+if (d==3)
+printf("uc\n");
+if (d==4)
+printf("dort\n");
+if (d==5)
+printf("bes\n");
+if (e==1)
+printf("bir\n");
+if (e==2)
+printf("iki\n");
+if (e==3)
+printf("uc\n");
+if (e==4)
+printf("dort\n");
+if (e==5)
+printf("bes\n");
+getch ();
+}*/
---
+++ msceee.cpp
@@ -0,0 +1,61 @@
+# include "stdio.h"
+# include "conio.h"
+void main()
+/*15.10.2014*/
+// BU PROGRAM HESAP  YAPAR HÜSEYÝN
+/*{
+	int x,y,z,sonuc;
+	printf("Sayilari girin\n");
+	scanf("%d %d %d",&x,&y,&z);
+	sonuc=x*y*z;
+	printf("Islemin sonucu = %d",sonuc);
+	getch ();
+}*/
+
+// BU PROGRAM FAIZ HESABI YAPAR HÜSEYÝN
+/*{
+float anapara,faizoran,yil,faiz;
+printf("Anaparayi girin =");
+scanf("%f",&anapara);
+printf("Faiz oranini girin = ");
+scanf("%f",&faizoran);
+printf("Yil girin = ");
+scanf("%f",&yil);
+faiz=anapara/100*faizoran*yil;
+printf("Hesap sonucu = %5.2f",faiz);
+getch ();
+}
+*/
+ // BU PROGRAM ÇARPIM YAPAR VE ÝÞLEMÝ GÖSTERÝR
+/*{
+		int x,y,z,sonuc;
+	printf("Sayilari girin\n");
+	scanf("%d %d %d",&x,&y,&z);
+	sonuc=x*y*z;
+	printf("%d*%d*%d = %d",x,y,z,sonuc);
+	getch ();
+}*/
+
+// BU PROGRAM NOTLARI AÐIRLIGINA GÖRE HESAPLAR ORTALAMA BULUR
+ /*{
+	float y1,y2,y3,y4,n1,n2,n3,n4,ort;
+	printf("Ilk sinav agirligi girin =");
+	scanf("%f",&y1);
+	printf("Birinci notu girin =");
+	scanf("%f",&n1);
+	printf("Ikinci sinav agirligi girin =");
+	scanf("%f",&y2);
+	printf("Ýkinci notu girin =");
+	scanf("%f",&n2);
+	printf("Ucuncu sinav agirligi girin =");
+	scanf("%f",&y3);
+	printf("Ucuncu notu girin =");
+	scanf("%f",&n3);
+	printf("Dorduncu sinav agirligi girin =");
+	scanf("%f",&y4);
+	printf("Dorduncu notu girin =");
+	scanf("%f",&n4);
+	ort=(y1*n1+y2*n2+y3*n3+n4*y4)/100;
+	printf("Ortalama = %f",ort);
+	getch();
+}*/
